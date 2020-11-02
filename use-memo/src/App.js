ef6843223fb9ceaf26c55b1bf3dd1f58b5381de6
Yao Zhang <Yao.Zhang@gartner.com>
2020-11-02 15:33:07 -0500

    the first set up nothing to useMemo

---
+++ use-memo/src/App.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+export default function App(){
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] =  useState(false);
+  const doubleNumber = slowFunction(number);
+  const themeStyles ={
+    backgroundColor :dark? 'black' : 'white',
+    color: dark? 'white':'black'
+  }
+
+  return (
+    <>
+    <input type='number' value={number} onChange={e=>setNumber(parseInt(e.target.value))}/>
+    <button onClick={()=>setDark(prevDark =>!prevDark)}> Change Theme</button>
+    <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  )
+}
+
+const slowFunction = (number)=>{
+  console.log('Calling Slow Function')
+  for(let i=0; i<=1000000000;i++){}
+  return number*2
+}
+
+
+
+
