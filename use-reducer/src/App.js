cb3adbe6b001eb8baecfad1269ab423fbd0a016f
Yao Zhang <Yao.Zhang@gartner.com>
2022-03-21 17:07:48 -0400

    finished the useReducer

--- use-reducer/src/App.js
+++ use-reducer/src/App.js
@@ -1,27 +1,64 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
+import Todo from './Todo.js';
+
+export const ACTIONS ={
+  // INCREMENT:'increment',
+  // DECREMENT:'decrement'
+  ADD_TODO:"add-todo",
+  TOGGLE_TODO:"toggle-todo",
+  DELETE_TODO:"delete-todo"
+}
+
+function reducer(todos, action){
+  // switch(action.type){
+  //   case ACTIONS.INCREMENT:
+  //     return {count:count+1}
+  //   case ACTIONS.DECREMENT:
+  //     return {count:count-1}
+  //   default:
+  //     return state;
+  // }
+  switch(action.type){
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo =>{
+        if(todo.id === action.payload.id){
+          return {...todo, complete:!todo.complete};
+        }else{
+          return todo;
+        }
+      })
+      case ACTIONS.DELETE_TODO:
+        return todos.filter(todo => todo.id!==action.payload.id)
+      default:
+        return todos
+  }
+}
+
+function newTodo(name){
+  return {id:Date.now(), name:name , complete:false}
+}
 
 const App=()=> {
+  const [todos, dispatch] = useReducer(reducer,[])
+  const [name, setName] = useState('');
 
-  const [count, setCount] = useState(4);
-
-
-  const decrementCount=()=>{
-    // setCount(count-1);
-    setCount(prevCount => prevCount-1);
-    return count;
+  function handleSubmit(e){
+    e.preventDefault();
+    dispatch({type: ACTIONS.ADD_TODO, payload:{name:name}})
+    setName('');
   }
-
-  const incrementCount=()=>{
-    setCount(prevCount => prevCount+1);
-  }
-  
   return (
     <>
-    <button onClick={decrementCount}>-</button>
-    <span>{count}</span>
-    <span>{theme}</span>
-    <button onClick={incrementCount}>+</button>
-    <span onClick={handleWordClick}>{word}</span>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={e=>setName(e.target.value)} />
+      </form>
+      {
+        todos.map(todo =>{
+         return  <Todo key={todo.id} todo={todo} dispatch ={dispatch}/>
+        })
+      }
     </>
   );
 }
